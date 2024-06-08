63a4d4690847e0130a36b317c70dc8639535fc57
kingstar <admin@baks.dev>
2024-06-08 20:58:49 +0300

    Baks Development

--- Resources/packages/yandex-market-orders/translation.php
+++ Resources/packages/yandex-market-orders/translation.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -23,11 +23,13 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use Symfony\Config\TwigConfig;
+use BaksDev\Yandex\Market\Orders\BaksDevYandexMarketOrdersBundle;
+use Symfony\Config\FrameworkConfig;
 
-return static function(TwigConfig $config, ContainerConfigurator $configurator) {
-	
-	$config->path(__DIR__.'/../view', 'yandex-market-orders');
+return static function(FrameworkConfig $config) {
+
+    $config
+        ->translator()
+        ->paths([BaksDevYandexMarketOrdersBundle::PATH.'Resources/translations/']);
 
 };
-
--- Resources/packages/yandex-market-orders/twig.php
+++ Resources/packages/yandex-market-orders/twig.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,27 +23,15 @@
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-return static function(ContainerConfigurator $configurator) {
+use BaksDev\Yandex\Market\Orders\BaksDevYandexMarketOrdersBundle;
+use Symfony\Config\TwigConfig;
 
-    $services = $configurator->services()
-        ->defaults()
-        ->autowire()
-        ->autoconfigure()
-    ;
+return static function(TwigConfig $twig) {
 
-    $NAMESPACE = 'BaksDev\Yandex\Market\Orders\\';
+    $twig->path(
+        BaksDevYandexMarketOrdersBundle::PATH.'Resources/view',
+        'yandex-market-orders'
+    );
 
-    $MODULE = substr(__DIR__, 0, strpos(__DIR__, "Resources"));
-
-    $services->load($NAMESPACE, $MODULE)
-        ->exclude([
-            $MODULE.'{Entity,Resources,Type}',
-            $MODULE.'**/*Message.php',
-            $MODULE.'**/*DTO.php',
-        ])
-    ;
-
-    $services->load($NAMESPACE.'Type\DeliveryType\\', $MODULE.'Type/DeliveryType');
-    $services->load($NAMESPACE.'Type\ProfileType\\', $MODULE.'Type/ProfileType');
-    $services->load($NAMESPACE.'Type\PaymentType\\', $MODULE.'Type/PaymentType');
 };
+

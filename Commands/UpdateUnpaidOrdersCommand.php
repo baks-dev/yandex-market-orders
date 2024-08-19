1ef66a057505fa287afb4a32d08893ae8c117789
kingstar <admin@baks.dev>
2024-08-19 10:07:01 +0300

    Baks Development

---
+++ Commands/UpdateUnpaidOrdersCommand.php
@@ -0,0 +1,157 @@
+<?php
+/*
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Yandex\Market\Orders\Commands;
+
+use BaksDev\Orders\Order\Entity\Order;
+use BaksDev\Orders\Order\Repository\ExistsOrderNumber\ExistsOrderNumberInterface;
+use BaksDev\Users\Profile\UserProfile\Type\Id\UserProfileUid;
+use BaksDev\Yandex\Market\Orders\Api\YaMarketNewOrdersRequest;
+use BaksDev\Yandex\Market\Orders\Api\YaMarketUnpaidOrdersRequest;
+use BaksDev\Yandex\Market\Orders\UseCase\New\YandexMarketOrderDTO;
+use BaksDev\Yandex\Market\Orders\UseCase\New\YandexMarketOrderHandler;
+use BaksDev\Yandex\Market\Orders\UseCase\Unpaid\UnpaidYaMarketOrderHandler;
+use BaksDev\Yandex\Market\Repository\AllProfileToken\AllProfileYaMarketTokenInterface;
+use DateInterval;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'baks:yandex-market-orders:unpaid',
+    description: 'Получает неоплаченные заказы Yandex Market'
+)]
+class UpdateUnpaidOrdersCommand extends Command
+{
+    private SymfonyStyle $io;
+
+    public function __construct(
+        private readonly AllProfileYaMarketTokenInterface $allProfileYaMarketToken,
+        private readonly YaMarketUnpaidOrdersRequest $yandexMarketUnpaidOrdersRequest,
+        private readonly UnpaidYaMarketOrderHandler $unpaidYaMarketOrderHandler,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->addArgument('argument', InputArgument::OPTIONAL, 'Описание аргумента');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->io = new SymfonyStyle($input, $output);
+
+        /** Получаем активные токены авторизации профилей Yandex Market */
+        $profiles = $this->allProfileYaMarketToken
+            ->onlyActiveToken()
+            ->findAll();
+
+        $profiles = iterator_to_array($profiles);
+
+        $helper = $this->getHelper('question');
+
+        $questions[] = 'Все';
+
+        foreach($profiles as $quest)
+        {
+            $questions[] = $quest->getAttr();
+        }
+
+        $question = new ChoiceQuestion(
+            'Профиль пользователя',
+            $questions,
+            0
+        );
+
+        $profileName = $helper->ask($input, $output, $question);
+
+        if($profileName === 'Все')
+        {
+            /** @var UserProfileUid $profile */
+            foreach($profiles as $profile)
+            {
+                $this->update($profile);
+            }
+        }
+        else
+        {
+            $UserProfileUid = null;
+
+            foreach($profiles as $profile)
+            {
+                if($profile->getAttr() === $profileName)
+                {
+                    /* Присваиваем профиль пользователя */
+                    $UserProfileUid = $profile;
+                    break;
+                }
+            }
+
+            if($UserProfileUid)
+            {
+                $this->update($UserProfileUid);
+            }
+
+        }
+
+        $this->io->success('Заказы успешно обновлены');
+
+        return Command::SUCCESS;
+    }
+
+    public function update(UserProfileUid $profile): void
+    {
+        $this->io->note(sprintf('Обновляем неоплаченные заказы профиля %s', $profile->getAttr()));
+
+        $orders = $this->yandexMarketUnpaidOrdersRequest
+            ->profile($profile)
+            ->findAll(DateInterval::createFromDateString('1 day'));
+
+        if($orders->valid())
+        {
+            /** @var YandexMarketOrderDTO $order */
+            foreach($orders as $order)
+            {
+                /**
+                 * Создаем системный заказ
+                 */
+                $handle = $this->unpaidYaMarketOrderHandler->handle($order);
+
+                if($handle instanceof Order)
+                {
+                    $this->io->info(sprintf('Добавили неоплаченный заказ %s', $order->getNumber()));
+                    continue;
+                }
+
+                $this->io->error(sprintf('%s: Ошибка при добавлении заказа %s', $handle, $order->getNumber()));
+            }
+        }
+    }
+}

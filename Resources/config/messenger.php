2f5c722afb1c93d75423d8d5c4b752b8e3158cc4
kingstar <admin@baks.dev>
2025-01-16 23:05:25 +0300

    Baks Development

--- Resources/config/messenger.php
+++ Resources/config/messenger.php
@@ -30,7 +30,7 @@ return static function(FrameworkConfig $framework) {
     $messenger = $framework->messenger();
 
     $messenger
-        ->transport('')
+        ->transport('yandex-market-orders')
         ->dsn('redis://%env(REDIS_PASSWORD)%@%env(REDIS_HOST)%:%env(REDIS_PORT)%?dbindex=%env(REDIS_TABLE)&auto_setup=true')
         ->options(['stream' => 'yandex-market-orders'])
         ->failureTransport('failed-yandex-market-orders')
